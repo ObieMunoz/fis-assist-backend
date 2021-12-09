b6bc418f97ef138e356edc4e5170d07f34cd5ce0
Obie <obiemunozjr@gmail.com>
2021-12-09 14:14:21 -0500

    rm db, schema, add gitignore

--- db/seeds/03_module_seed.rb
+++ db/seeds/03_module_seed.rb
@@ -6,9 +6,19 @@ Mod.create(title: 'CLI', course_id: 1)
 Mod.create(title: 'Git', course_id: 1)
 Mod.create(title: 'HTML', course_id: 1)
 Mod.create(title: 'CSS', course_id: 1)
-Mod.create(title: 'Programming as Conversation Part 1: Expressions', course_id: 1)
-Mod.create(title: 'Programming as Conversation Part 2: Statements', course_id: 1)
-Mod.create(title: 'Programming as Conversation Part 3: Bundling Expressions and Statements into Functions', course_id: 1)
+Mod.create(
+  title: 'Programming as Conversation Part 1: Expressions',
+  course_id: 1,
+)
+Mod.create(
+  title: 'Programming as Conversation Part 2: Statements',
+  course_id: 1,
+)
+Mod.create(
+  title:
+    'Programming as Conversation Part 3: Bundling Expressions and Statements into Functions',
+  course_id: 1,
+)
 Mod.create(title: 'Working with Data Structures', course_id: 1)
 Mod.create(title: 'Manipulating the DOM', course_id: 1)
 Mod.create(title: 'JavaScript Events', course_id: 1)
@@ -22,7 +32,10 @@ Mod.create(title: 'Scope', course_id: 2)
 Mod.create(title: 'Working with Data Scructures in JavaScript', course_id: 2)
 Mod.create(title: 'Array Iteration', course_id: 2)
 Mod.create(title: 'JavaScript Advanced Syntax', course_id: 2)
-Mod.create(title: "Introducing Flatiron's Three Pillars of JavaScript", course_id: 2)
+Mod.create(
+  title: "Introducing Flatiron's Three Pillars of JavaScript",
+  course_id: 2,
+)
 Mod.create(title: 'JavaScript and the DOM', course_id: 2)
 Mod.create(title: 'JavaScript Events', course_id: 2)
 Mod.create(title: 'Communicating with the Server', course_id: 2)
@@ -32,7 +45,10 @@ Mod.create(title: 'Algorithmic Problem Solving', course_id: 2)
 Mod.create(title: 'Context in JavaScript', course_id: 2)
 Mod.create(title: 'Object-Oriented JavaScript', course_id: 2)
 Mod.create(title: 'JavaScript Inheritance', course_id: 2)
-Mod.create(title: 'Additional Practice: HTML Forms and Modern Elements', course_id: 2)
+Mod.create(
+  title: 'Additional Practice: HTML Forms and Modern Elements',
+  course_id: 2,
+)
 
 # ------------PHASE TWO (2)-------------
 
@@ -54,7 +70,10 @@ Mod.create(title: 'Configuring Ruby Applications', course_id: 4)
 Mod.create(title: '[Optional] Regex', course_id: 4)
 Mod.create(title: 'Introduction to Object-Orientation in Ruby', course_id: 4)
 Mod.create(title: 'Topics in OO - Self in Ruby', course_id: 4)
-Mod.create(title: 'Topics in OO - Class Variables and Methods in Ruby', course_id: 4)
+Mod.create(
+  title: 'Topics in OO - Class Variables and Methods in Ruby',
+  course_id: 4,
+)
 Mod.create(title: 'Topics in OO - Object Inheritance in Ruby', course_id: 4)
 Mod.create(title: 'Object-Relational Mapping', course_id: 4)
 Mod.create(title: '[Optional] Advanced ORM Methods', course_id: 4)
